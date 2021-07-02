a2cfbe5b4561106f20e208a72e6cadd431de90b6
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2021-07-02 10:37:19 +0100

    code added for tutorial

--- concatenate-two-variables.js
+++ concatenate-two-variables.js
@@ -1,1 +1,35 @@
+// Addition Operator
 
+const first_name = 'ravinder';
+const last_name = 'deol';
+
+const full_name = first_name + ' ' + last_name;
+
+console.log(full_name);
+
+// Template Literals
+
+const first_name = 'ravinder';
+const last_name = 'deol';
+
+const full_name = `${first_name} ${last_name}`;
+
+console.log(full_name);
+
+// Concat Method
+
+const first_name = 'ravinder';
+const last_name = 'deol';
+
+const full_name = first_name.concat(' ', last_name);
+
+console.log(full_name);
+
+// Join Method
+
+const first_name = 'ravinder';
+const last_name = 'deol';
+
+const full_name = [first_name, last_name].join(' ');
+
+console.log(full_name);
