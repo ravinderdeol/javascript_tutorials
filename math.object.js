204273b2a965324b75e5c24b7843a1e19c8488e2
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2021-08-05 10:02:16 +0100

    updated comments

--- math.object.js
+++ math.object.js
@@ -1,7 +1,5 @@
-// floor() rounds down to the nearest integer
-floor = Math.floor(1.5);
+floor = Math.floor(1.5); // rounds down to the nearest integer
 console.log(floor); // returns 1
 
-// trunc() returns the integer part of a number
-trunc = Math.trunc(1.5);
+trunc = Math.trunc(1.5); // returns the integer part of a number
 console.log(trunc); // returns 1
