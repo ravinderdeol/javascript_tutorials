bc5e3bd6a09307fa104c300d8ee6609f82cd901b
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2021-08-05 10:07:29 +0100

    round up tuts added

--- math.object.js
+++ math.object.js
@@ -3,3 +3,9 @@ console.log(floor); // returns 1
 
 trunc = Math.trunc(1.5); // returns the integer part of a number
 console.log(trunc); // returns 1
+
+round = Math.round(1.5); // rounds to the nearest integer
+console.log(round); // returns 2
+
+ceil = Math.ceil(1.5); // rounds up to the nearest integer
+console.log(ceil); // returns 2
