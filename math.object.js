de35a65e1752921df3556de40cab8da51fe07d96
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2021-08-05 10:01:16 +0100

    math object file created

---
+++ math.object.js
@@ -0,0 +1,7 @@
+// floor() rounds down to the nearest integer
+floor = Math.floor(1.5);
+console.log(floor); // returns 1
+
+// trunc() returns the integer part of a number
+trunc = Math.trunc(1.5);
+console.log(trunc); // returns 1
